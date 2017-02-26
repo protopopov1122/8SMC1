44e343902ea82593e7d0fa477506fec0d79f3c63
Eugene P <protopopov1122@yandex.ru>
2017-02-26 18:50:01 +0200

    Organise Windows build.

--- winbuild/build-stub.bat
+++ winbuild/build-stub.bat
@@ -1,2 +1,4 @@
 @echo off
 cl /W0 /EHsc /I res /Zi /Gm /LD /FeUSMCDLL.dll misc/Stub.cpp /link /MACHINE:X86
+move USMCDLL.dll build
+move USMCDLL.lib build
--- winbuild/build-ui.bat
+++ winbuild/build-ui.bat
@@ -1,3 +1,4 @@
 @echo off
-cl /EHsc /Zi /Gm /std:c++latest /W0 /I. /I res /I %1\include /I %1\lib\vc_dll\mswud /Feui.exe ui/app.cpp ui/mainFrame.cpp  /link %1\lib\vc_dll\wxmsw31u.lib USMCDLL.lib 8smc1.lib /MACHINE:x86
-copy %1\lib\vc_dll\wxmsw310u_vc_custom.dll .
+cl /EHsc /Zi /Gm /std:c++latest /W0 /I. /I res /I %1\include /I %1\lib\vc_dll\mswud /Feui.exe ui/app.cpp ui/mainFrame.cpp  /link %1\lib\vc_dll\wxmsw31u.lib build\USMCDLL.lib build\8smc1.lib /MACHINE:x86
+copy %1\lib\vc_dll\wxmsw310u_vc_custom.dll build
+move ui.exe build
