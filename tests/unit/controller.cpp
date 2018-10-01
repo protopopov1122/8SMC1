5b888cd0dd04cee16bb61a7aaf531de5956b53fa
J. Protopopovs <jprotopopov1122@gmail.com>
2018-10-01 16:51:38 +0300

    Add more tests

--- tests/unit/controller.cpp
+++ tests/unit/controller.cpp
@@ -1,15 +1,17 @@
 #include "catch.hpp"
 #include "ctrl-lib/ctrl/MotorController.h"
+#include "ctrl-lib/ctrl/InstrumentController.h"
 #include "device/emulated/EmuDeviceManager.h"
 
 using namespace CalX;
 
 #define REQUIRE_NOERR(x) REQUIRE((x) == ErrorCode::NoError)
 
-TEST_CASE("Motor controller state", "[controller]") {
+TEST_CASE("Motor controller", "[controller]") {
   EmuDeviceManager devman;
   ConfigManager config;
   Motor *motor = devman.connectMotor(nullptr);
+  REQUIRE(motor != nullptr);
   MotorController controller(config, *motor);
   controller.enablePower(true);
   SECTION("Controller-motor proxies") {
@@ -53,5 +55,76 @@ TEST_CASE("Motor controller state", "[controller]") {
     REQUIRE_NOERR(controller.startMove(0, 0.0f));
     REQUIRE(!(controller.isTrailerPressed(TrailerId::Trailer2) || controller.isTrailerPressed(TrailerId::Trailer2)));
     REQUIRE_NOERR(controller.checkTrailers());
+  }
+  SECTION("Motor events") {
+    // TODO Add motor event tests
+  }
+}
+
+TEST_CASE("Instrument controller", "[controller]") {
+  EmuDeviceManager devman;
+  ConfigManager config;
+  Instrument *instrument = devman.connectInstrument(nullptr);
+  REQUIRE(instrument != nullptr);
+  InstrumentController controller(config, *instrument);
+  SECTION("Instrument parameters") {
+    REQUIRE(&controller.getDevice() == instrument);
+    REQUIRE(&controller.getInstrument() == instrument);
+    REQUIRE(controller.getID() == instrument->getID());
+    REQUIRE(controller.isEnabled() == instrument->enabled());
+    REQUIRE(controller.getMode() == instrument->getWorkingMode());
+    REQUIRE(controller.getInfo().compare(instrument->getDeviceInfo()) == 0);
+  }
+  SECTION("Instrument state management") {
+    REQUIRE(!controller.isEnabled());
+    REQUIRE_NOERR(controller.enable(true));
+    REQUIRE((controller.isEnabled() && instrument->enabled()));
+    REQUIRE_NOERR(controller.enable(true));
+    REQUIRE(controller.isEnabled());
+    REQUIRE_NOERR(controller.flipState());
+    REQUIRE((!controller.isEnabled() && !instrument->enabled()));
+    REQUIRE_NOERR(controller.flipState());
+    REQUIRE(controller.isEnabled());
+    REQUIRE_NOERR(controller.enable(false));
+    REQUIRE((!controller.isEnabled() && !instrument->enabled()));
+  }
+  SECTION("Instrument session management") {
+    REQUIRE(!controller.isSessionOpened());
+    REQUIRE_NOERR(controller.open_session());
+    REQUIRE(controller.isSessionOpened());
+    REQUIRE_NOERR(controller.open_session());
+    REQUIRE_NOERR(controller.open_session());
+    REQUIRE(controller.isSessionOpened());
+    REQUIRE_NOERR(controller.close_session());
+    REQUIRE(!controller.isSessionOpened());
+    REQUIRE_NOERR(controller.close_session());
+    REQUIRE_NOERR(controller.close_session());
+    REQUIRE(!controller.isSessionOpened());
+  }
+  SECTION("Instrument states & sessions") {
+    REQUIRE((!controller.isSessionOpened() && !controller.isEnabled()));
+    REQUIRE_NOERR(controller.open_session());
+    REQUIRE((controller.isSessionOpened() && !controller.isEnabled()));
+    REQUIRE_NOERR(controller.close_session());
+    REQUIRE((!controller.isSessionOpened() && !controller.isEnabled()));
+    REQUIRE_NOERR(controller.enable(true));
+    REQUIRE((controller.isSessionOpened() && controller.isEnabled()));
+    REQUIRE_NOERR(controller.enable(false));
+    REQUIRE((controller.isSessionOpened() && !controller.isEnabled()));
+    REQUIRE_NOERR(controller.flipState());
+    REQUIRE_NOERR(controller.close_session());
+    REQUIRE((!controller.isSessionOpened() && !controller.isEnabled()));
+  }
+  SECTION("Instrument modes") {
+    REQUIRE(controller.getMode() == InstrumentMode::Off);
+    REQUIRE(controller.setMode(InstrumentMode::Prepare));
+    REQUIRE(controller.getMode() == InstrumentMode::Prepare);
+    REQUIRE(controller.setMode(InstrumentMode::Full));
+    REQUIRE(controller.getMode() == InstrumentMode::Full);
+    REQUIRE(controller.setMode(InstrumentMode::Off));
+    REQUIRE(controller.getMode() == InstrumentMode::Off);
+  }
+  SECTION("Instrument events") {
+    // TODO Add instrument event tests
   }
 }
---
+++ tests/unit/gcode.cpp
@@ -0,0 +1,8 @@
+#include "catch.hpp"
+#include "ctrl-lib/gcode/GCodeStream.h"
+
+using namespace CalX;
+
+TEST_CASE("GCode stream") {
+  
+}
